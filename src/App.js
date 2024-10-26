c19a5ac020d40554f31dfb1ba0154085dad4fdd7
articoding <144624806+articoding@users.noreply.github.com>
2024-10-26 17:37:09 -0500

    Refactor font configuration and import in index.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './views/auth/LoginForm'; // Adjusted path to the LoginForm
 import SignupForm from './views/auth/SignUpForm';
 import Dashboard from './views/admin/Dashboard';
+import './index.css'; // Make sure to import the CSS file where the font is defined
+
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Use Poppins as the default font
+      },
+    },
   },
   plugins: [],
-}
+};
